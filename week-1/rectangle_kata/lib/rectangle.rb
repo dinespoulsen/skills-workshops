5d79de3b145cd62d076820f79bfc3e9bb234ce69
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-07 19:49:01 +0000

    Added parameter features to rectangle

--- week-1/rectangle_kata/lib/rectangle.rb
+++ week-1/rectangle_kata/lib/rectangle.rb
@@ -1,3 +1,19 @@
-def rectangle
-  "THIS SHOULD BE A RECTANGLE!"
+def rectangle(w = 0, h = 0)
+  return "Rectangle does not exist" if w <= 0 || h <= 0
+  w > 1 ? ending = "|\n" : ending = "\n"
+  w > 1 ? correct = 2 : correct = 1
+  width = "|" + "-"*(w - correct) + ending
+  height = "|" + " "*(w - correct) +ending
+  if h < 2
+    rectangle = width
+  elsif
+    h == 2
+    rectangle = width + width
+  else
+  rectangle = width + height*(h-2) + width
+  end
+  rectangle
 end
+
+
+print rectangle(1, 1)
--- week-1/rectangle_kata/spec/rectangle_spec.rb
+++ week-1/rectangle_kata/spec/rectangle_spec.rb
@@ -2,7 +2,7 @@ require "rectangle"
 
 describe "returning a 20x10 rectangle" do
   it "returns a rectangle of the right size" do
-    expect(rectangle).to eq("|------------------|\n" +
+    expect(rectangle(20, 10)).to eq("|------------------|\n" +
                             "|                  |\n" +
                             "|                  |\n" +
                             "|                  |\n" +
@@ -12,5 +12,21 @@ describe "returning a 20x10 rectangle" do
                             "|                  |\n" +
                             "|                  |\n" +
                             "|------------------|\n")
+  end
+end
+
+describe "returning a 20x5 rectangle" do
+  it "returns a rectangle of the right size" do
+    expect(rectangle(20, 5)).to eq("|------------------|\n" +
+                            "|                  |\n" +
+                            "|                  |\n" +
+                            "|                  |\n" +
+                            "|------------------|\n")
+  end
+end
+
+describe "returning a 1x1 rectangle" do
+  it "returns a rectangle of the right size" do
+    expect(rectangle(1, 1)).to eq("|\n")
   end
 end
